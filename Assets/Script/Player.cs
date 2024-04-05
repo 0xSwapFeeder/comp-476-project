70b487d4a9db755c31046ae51ed9431bece0ebf7
Lucsc <luc1.schmitt@epitech.eu>
2024-04-05 19:26:09 -0400

    [ADD] basic player with broom and quidditch field

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Basic movement with input horizontal and vertical
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+        
+        
+    }
+}
