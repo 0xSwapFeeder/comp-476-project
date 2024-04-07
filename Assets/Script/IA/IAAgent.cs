c68f698f8cf9ab5dc1524cd99eb101fd3f45fe63
0xSwapFeeder <tristan.gremont@epitech.eu>
2024-04-07 15:05:47 -0400

    feat(IA): added all players agents

--- Assets/Script/IA/IAAgent.cs
+++ Assets/Script/IA/IAAgent.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class IAAgent : MonoBehaviour
@@ -25,5 +25,27 @@ public class IAAgent : MonoBehaviour
     void Update()
     {
         movement.Avoid();
+    }
+
+    public void FlockWithSameTeamMemberType()
+    {
+        var sameTeamMembers = GetSameTeamTypes();
+        var averagePosition = sameTeamMembers.Aggregate(Vector3.zero, (current, member) => current + member.transform.position) / sameTeamMembers.Count;
+        var averageVelocity = sameTeamMembers.Aggregate(Vector3.zero, (current, member) => current + member.movement.Velocity) / sameTeamMembers.Count;
+        var direction = averagePosition - transform.position;
+        movement.Velocity += direction.normalized;
+    }
+
+    public List<IAAgent> GetSameTeamTypes()
+    {
+        var teamMembers = GetTeamMembers();
+        return teamMembers.Where(t => t.type == type).ToList();
+    }
+    
+    public List<IAAgent> GetTeamMembers()
+    {
+        return team == Team.Red 
+            ? GameObject.FindGameObjectsWithTag("RedTeam").Select(g => g.GetComponent<IAAgent>()).ToList()
+            : GameObject.FindGameObjectsWithTag("BlueTeam").Select(g => g.GetComponent<IAAgent>()).ToList();
     }
 }
