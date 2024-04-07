26c8e7dae3a82654e8364e5319c59b8dc168c192
0xSwapFeeder <tristan.gremont@epitech.eu>
2024-04-07 15:14:04 -0400

    feat(IA): added Balls scripts and started Beater logic

--- Assets/Script/IA/IABeaterAgent.cs
+++ Assets/Script/IA/IABeaterAgent.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class IABeaterAgent : IAAgent
 {
+    Bludger currentBludger;
+
+    public float throwingDistance;
+    public float throwingForce;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,19 @@ public class IABeaterAgent : IAAgent
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentBludger == null) {
+            
+        }
     }
+
+    bool hasToDefendTeamFromIncomingBludger()
+    {
+        return false;
+    }
+    
+    List<Bludger> getBludgers()
+    {
+        return GameObject.FindGameObjectsWithTag("Bludger").Select(g => g.GetComponent<Bludger>()).ToList();
+    }
+    
 }
