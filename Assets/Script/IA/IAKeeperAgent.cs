4c3dba2b75223d312dbcdba383220839afb5b96c
0xSwapFeeder <tristan.gremont@epitech.eu>
2024-04-09 15:29:50 -0400

    feat(IA): added Keeper logic

--- Assets/Script/IA/IAKeeperAgent.cs
+++ Assets/Script/IA/IAKeeperAgent.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IAKeeperAgent : MonoBehaviour
+public class IAKeeperAgent : IAAgent
 {
+    public float distanceToGoal;
+    public float defenseDistance;
+    public float catchingDistance;
+
+    
+    private Quaffle quaffle;
+    private Transform goal;
+    private Transform oppositeGoal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var goal1 = GameObject.FindGameObjectWithTag("GoalFirst");
+        var goal2 = GameObject.FindGameObjectWithTag("GoalSecond");
+        goal = Vector3.Distance(transform.position, goal1.transform.position) < Vector3.Distance(transform.position, goal2.transform.position) ? goal1.transform : goal2.transform;
+        oppositeGoal = goal == goal1.transform ? goal2.transform : goal1.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        setClosestQuaffle();
+        if (quaffle != null && Vector3.Distance(transform.position, quaffle.transform.position) < defenseDistance)
+        {
+            transform.LookAt(quaffle.transform);
+            movement.Target = quaffle.transform;
+            movement.Pursue();
+        }
+        else if (Vector3.Distance(transform.position, goal.position) > distanceToGoal)
+        {
+            transform.LookAt(goal);
+            movement.Target = goal;
+            movement.Arrive();
+        }
+        else if (quaffle != null && Vector3.Distance(transform.position, quaffle.transform.position) < catchingDistance)
+        {
+            quaffle.PickUp(transform);
+            quaffle.GetThrown(oppositeGoal);
+        }
+    }
+
+    void setClosestQuaffle()
+    {
+        GameObject[] quaffles = GameObject.FindGameObjectsWithTag("Quaffle");
+        float minDistance = float.MaxValue;
+        foreach (var quaffle in quaffles)
+        {
+            float distance = Vector3.Distance(transform.position, quaffle.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                this.quaffle = quaffle.GetComponent<Quaffle>();
+            }
+        }
     }
 }
