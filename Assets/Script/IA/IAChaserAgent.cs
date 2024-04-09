f023725f93e1f3f746f869f7cdb063b3b9895d64
0xSwapFeeder <tristan.gremont@epitech.eu>
2024-04-09 15:37:03 -0400

    feat(AI): added Chaser logic

--- Assets/Script/IA/IAChaserAgent.cs
+++ Assets/Script/IA/IAChaserAgent.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class IAChaserAgent : IAAgent
 {
+    Quaffle currentQuaffle;
+    public float catchingDistance;
+    public float throwingDistance;
+    private bool hasQuaffle;
+    private Transform oppositeGoal;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        oppositeGoal = getOppositeGoal();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasQuaffle)
+        {
+            movement.Arrive();
+            if (Vector3.Distance(transform.position, oppositeGoal.position) <= throwingDistance)
+            {
+                currentQuaffle.GetThrown(oppositeGoal);
+                hasQuaffle = false;
+            }
+            return;
+        }
+        setClosestQuaffle();
+        movement.Target = currentQuaffle.transform;
+        movement.Pursue();
+        if (Vector3.Distance(transform.position, currentQuaffle.transform.position) < catchingDistance && currentQuaffle.transform.parent == null)
+        {
+            currentQuaffle.PickUp(transform);
+            hasQuaffle = true;
+            movement.Target = oppositeGoal;
+        }
+    }
+    
+    void setClosestQuaffle()
+    {
+        GameObject[] quaffles = GameObject.FindGameObjectsWithTag("Quaffle");
+        float minDistance = float.MaxValue;
+        foreach (var quaffle in quaffles)
+        {
+            float distance = Vector3.Distance(transform.position, quaffle.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                currentQuaffle = quaffle.GetComponent<Quaffle>();
+            }
+        }
+    }
+    Transform getOppositeGoal()
+    {
+        var goal1 = GameObject.FindGameObjectWithTag("GoalFirst");
+        var goal2 = GameObject.FindGameObjectWithTag("GoalSecond");
+        return Vector3.Distance(transform.position, goal1.transform.position) < Vector3.Distance(transform.position, goal2.transform.position) ? goal2.transform : goal1.transform;
     }
 }
